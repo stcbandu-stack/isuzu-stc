b4009f4dff3de4340afabf4981f1355bba1d2e99
stcbandu-stack <stc.bandu@gmail.com>
2026-01-29 08:30:39 +0700

    fixed login session reject
    แก้ล็อคอินไม่ได้ เด้งกลับหน้าล็อคอิน

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [vue(), tailwind()],
 });
